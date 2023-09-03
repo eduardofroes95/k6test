3b376ffc6cb79af241f266d1a95485626b051940
Eduardo Froes <eduardofroes95@gmail.com>
2023-09-03 00:14:55 -0300

    Exemplos

--- k6.test.js
+++ k6.test.js
@@ -1,12 +1,22 @@
 import http from 'k6/http';
-
+import { check } from 'k6'; 
 import { sleep } from 'k6';
 
 
+export const options = {
+
+  vus: 5,
+  duration: '10s'
+
+}
+
+
+
 export default function () {
+const res = http.get('http://test.k6.io')
 
-  http.get('https://test.k6.io');
-
-  sleep(1);
+check(res,{
+  'Status code 200': (r) => r.status == 200
+})
 
 }
